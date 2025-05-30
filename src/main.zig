114b0bc36711a9b3ea2b23d2aff4aaa5985ca076
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2025-05-30 16:11:03 -0700

    main: remove timing prints

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,7 @@ fn usage() void {
         \\usage <options?> <file>
         \\
         \\options:
-        \\  --json-to-edn file is a json file to be converted to edn
+        \\  --json-to-edn: convert json file to edn
         \\
     , .{});
 }
@@ -24,7 +24,9 @@ pub fn main() !void {
 fn mainInner() !void {
     const alloc = std.heap.smp_allocator;
     const args = try std.process.argsAlloc(alloc);
-    if (args.len < 2) return error.Args;
+    defer std.process.argsFree(alloc, args);
+
+    if (args.len < 2) return error.ArgsLen;
     var bw = std.io.bufferedWriter(std.io.getStdOut().writer());
     const stdout = bw.writer();
 
@@ -34,30 +36,24 @@ fn mainInner() !void {
         defer file.close();
         const src = try file.readToEndAllocOptions(alloc, 1024 * 1024, null, 8, 0);
         defer alloc.free(src);
-        var timer = try std.time.Timer.start();
         const json = try std.json.parseFromSlice(std.json.Value, alloc, src, .{});
         defer json.deinit();
-        const t = std.fmt.fmtDuration(timer.lap());
         try edn.writeFromJson(json.value, stdout, .{ .whitespace = .indent_2 });
         try stdout.writeAll("\n");
         try bw.flush();
-        try std.io.getStdErr().writer().print("\nstd.json.parseFromSlice took {}\nedn.writeFromJson took {}\n", .{ t, std.fmt.fmtDuration(timer.lap()) });
         return;
     }
-    defer std.process.argsFree(alloc, args);
+
     const file = if (std.mem.eql(u8, args[1], "-")) std.io.getStdIn() else try std.fs.cwd().openFile(args[1], .{});
     defer file.close();
     const src = try file.readToEndAllocOptions(alloc, 1024 * 1024, null, 8, 0);
     defer alloc.free(src);
-    var timer = try std.time.Timer.start();
     var diag: edn.Diagnostic = .{ .file_path = args[1] };
     const result = edn.parseFromSlice(edn.Result, src, .{ .diagnostic = &diag, .allocator = alloc }, .{}) catch {
         try std.io.getStdErr().writer().print("{s}\n", .{diag.error_message});
         return;
     };
     defer result.deinit(alloc);
-    const t = std.fmt.fmtDuration(timer.lap());
     try stdout.print("{}\n", .{result.formatter(src)});
     try bw.flush();
-    try std.io.getStdErr().writer().print("\nedn.parseFromSlice took {}\n", .{t});
 }
